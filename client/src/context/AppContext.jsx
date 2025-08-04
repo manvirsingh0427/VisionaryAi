217a2e92d97df5e3d28659f5dcc5d330a9e7f9b3
manvirsingh0427 <manvirsinghmadhok@gmail.com>
2025-08-04 19:38:51 +0530

    Created Navbar for my text to image generator app

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import { useState } from "react";
+export const Appcontext = createContext()
+
+const AppContextProvider = (props) =>{
+     const [user, setUser] = useState(1);
+     const value = {
+        user, setUser
+     }
+
+     return (
+        <Appcontext.Provider value = {value}>
+            {props.children}
+        </Appcontext.Provider>
+     )
+}
+
+export default AppContextProvider
