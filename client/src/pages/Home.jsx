add8ac75abbab0e3a6a9c25ccca81083b574c749
manvirsingh0427 <manvirsinghmadhok@gmail.com>
2025-08-06 19:30:32 +0530

    Completed testimonial section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 
 import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
 const Home = () => {
   return (
     <div>
       <Header/>
       <Steps/>
       <Description/>
+      <Testimonials/>
     </div>
   )
 }
